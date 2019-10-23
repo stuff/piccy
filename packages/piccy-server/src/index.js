fe079e238092c96c41d0aad2cb7d54991ce1ddcc
Nicolas CHALLEIL <nchalleil@gmail.com>
2019-10-23 15:16:12 +0200

    feat(server): add scaled img route

--- packages/piccy-server/src/index.js
+++ packages/piccy-server/src/index.js
@@ -26,6 +26,25 @@ app.get('/img/:data', function(req, res) {
   canvas.pngStream().pipe(res);
 });
 
+app.get('/img/:scale/:data', function(req, res) {
+  const { size, imageData } = services.fromPalettizedData(req.params.data, 24);
+  const { scale } = req.params;
+
+  const sourceCanvas = createCanvas(size, size);
+  const sourceCtx = sourceCanvas.getContext('2d');
+
+  const canvas = createCanvas(size * scale, size * scale);
+  const ctx = canvas.getContext('2d');
+  ctx.imageSmoothingEnabled = false;
+
+  sourceCtx.putImageData(imageData, 0, 0);
+  // prettier-ignore
+  ctx.drawImage(sourceCanvas, 0, 0, size, size, 0, 0, size * scale, size * scale);
+
+  res.setHeader('Content-Type', 'image/png');
+  canvas.pngStream().pipe(res);
+});
+
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
   res.sendFile(WEBAPP_FOLDER);
