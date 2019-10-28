e07eddbbde4f9442fc933d61087b1c82d8e47a16
Nicolas CHALLEIL <nchalleil@gmail.com>
2019-10-28 18:22:32 +0100

    feat(editor): add right click draw

--- packages/piccy-editor/src/Palette.jsx
+++ packages/piccy-editor/src/Palette.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
 function Palette({ colors, onSelectColor, currentColor }) {
+  const clickHandler = e => {
+    e.preventDefault();
+    const { color } = e.target.dataset;
+    const colorIndex = e.type === 'contextmenu' ? 0 : 1;
+
+    onSelectColor(color, colorIndex);
+  };
   return (
     <div className="palette">
       {colors.map(color => (
         <div className="palette-item" key={color}>
           <button
+            data-color={color}
             className="palette-itemInner"
             style={{ background: color }}
-            onClick={() => {
-              onSelectColor(color);
-            }}
+            onClick={clickHandler}
+            onContextMenu={clickHandler}
           />
           {color === currentColor ? (
             <span className="palette-selector">▼</span>
