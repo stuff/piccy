e93a384902c2749421c9e621f25e031a6def086c
Nicolas CHALLEIL <nchalleil@gmail.com>
2019-10-31 19:59:51 +0100

    feat(server): try to compress image

--- packages/piccy-server/src/index.js
+++ packages/piccy-server/src/index.js
@@ -1,5 +1,5 @@
-// import LZString from 'lz-string';
 import path from 'path';
+import sharp from 'sharp';
 import express from 'express';
 import { createCanvas } from 'canvas';
 
@@ -10,26 +10,41 @@ const WEBAPP_FOLDER = path.join(__dirname + '../../../piccy-editor/build/');
 
 const app = express();
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.redirect('/edit');
 });
 
 app.use(express.static(WEBAPP_FOLDER));
 
-app.get('/img/:data', function(req, res) {
-  const { size, imageData } = services.fromPalettizedData(req.params.data, 24);
-  const canvas = createCanvas(size, size);
-  const ctx = canvas.getContext('2d');
-  ctx.putImageData(imageData, 0, 0);
-
-  res.setHeader('Content-Type', 'image/png');
-  canvas.pngStream().pipe(res);
+app.get('/img/:data', (req, res) => {
+  res.redirect(301, '/img/1/' + req.params.data);
 });
 
-function image(req, res) {
-  const { size, imageData } = services.fromPalettizedData(req.params.data, 24);
-  const { scale } = req.params;
+app.get('/img/:scale/:data', async (req, res) => {
+  const { size, imageData } = services.fromPalettizedData(req.params.data);
+  const { scale = 1 } = req.params;
 
+  const canvas = createCanvasFromImageData(imageData, size, scale);
+
+  res.setHeader('Content-Type', 'image/png');
+
+  const s = sharp(canvas.toBuffer());
+  const buffer = await s
+    .png({
+      palette: true,
+      colors: 16
+    })
+    .toBuffer();
+
+  res.send(buffer);
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(WEBAPP_FOLDER);
+});
+
+function createCanvasFromImageData(imageData, size, scale) {
   const sourceCanvas = createCanvas(size, size);
   const sourceCtx = sourceCanvas.getContext('2d');
 
@@ -38,18 +53,11 @@ function image(req, res) {
   ctx.imageSmoothingEnabled = false;
 
   sourceCtx.putImageData(imageData, 0, 0);
+
   // prettier-ignore
   ctx.drawImage(sourceCanvas, 0, 0, size, size, 0, 0, size * scale, size * scale);
 
-  res.setHeader('Content-Type', 'image/png');
-  canvas.pngStream().pipe(res);
+  return canvas;
 }
-
-app.get('/img/:scale/:data', image);
-
-// Handles any requests that don't match the ones above
-app.get('*', (req, res) => {
-  res.sendFile(WEBAPP_FOLDER);
-});
 
 app.listen(PORT, () => console.log(`Piccy Server on port ${PORT}!`));
