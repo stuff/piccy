574897f7ed4d1480d4c1635e87b5f2a1a545df40
Nicolas CHALLEIL <nchalleil@gmail.com>
2019-11-01 17:18:01 +0100

    fix(deploy): add securized shipitfile

---
+++ shipitfile.js
@@ -0,0 +1,48 @@
+var merge = require('lodash/merge');
+
+// sensible information for shipit are in that file :)
+var secret = require('./shipit-secret');
+
+module.exports = shipit => {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig(
+    merge(
+      {
+        default: {
+          repositoryUrl: 'git@github.com:stuff/piccy.git'
+        }
+      },
+      secret
+    )
+  );
+
+  shipit.blTask('install', async () => {
+    await shipit.remote(`
+        source ./.zshrc
+        echo "node: $(node -v)"
+        echo "yarn: $(yarn -v)"
+        echo " "
+        cd ./${shipit.releasePath}
+        yarn install
+    `);
+  });
+
+  shipit.blTask('build', async () => {
+    await shipit.remote(`
+        source ./.zshrc
+        cd ./${shipit.releasePath}
+        yarn workspace @stuff/piccy-editor build
+    `);
+  });
+
+  shipit.blTask('restart', async () => {
+    await shipit.remote(`
+        pm2 restart piccy
+    `);
+  });
+
+  shipit.on('deployed', () => {
+    shipit.start(['install', 'build', 'restart']);
+  });
+};
